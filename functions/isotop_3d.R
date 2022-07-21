74622f414609e76e7965ceae5ea262b88edefd98
zoometh <thomashuet7@gmail.com>
2022-07-21 17:54:29 +0100

    html widget datatable

--- functions/isotop_3d.R
+++ functions/isotop_3d.R
@@ -3,7 +3,7 @@
 #' @description get a dataframe and create a 3D plot
 #'
 #' @param df the dataframe. The color is displayed on the 'object' field
-#' @param colvar the names of the columns where the isotops measurments are
+#' @param vars the names of the columns where the isotops measurments are
 #' @param out.plot the path and name of the created 3D plot
 #'
 #' @return a plotly HML 3D plot
@@ -11,18 +11,24 @@
 #' @examples
 #'
 #' @export
-isotop_3d <- function(df, colvar = c("var1", "var2", "var3"),
+isotop_3d <- function(df,
+                      vars = c("var1", "var2", "var3"),
+                      color.column = "object",
                       out.plot = paste0(getwd(), "/data/isotop_3d.html")){
-
+  # print(colnames(df))
   d3 <- plotly::plot_ly(df,
-                        x = df[ , colvar[1]],
-                        y = df[ , colvar[2]],
-                        z = df[ , colvar[3]],
-                        color = ~color)
+                        x = df[ , vars[1]],
+                        y = df[ , vars[2]],
+                        z = df[ , vars[3]],
+                        color = ~color,
+                        hoverinfo = 'text',
+                        text = paste0(df[ , color.column]))
   d3 <- d3 %>% plotly::add_markers()
-  d3 <- d3 %>% plotly::layout(scene = list(xaxis = list(title = colvar[1]),
-                                           yaxis = list(title = colvar[2]),
-                                           zaxis = list(title = colvar[3])))
+  d3 <- d3 %>% plotly::layout(title = 'Isotop measures',
+                              scene = list(xaxis = list(title = vars[1]),
+                                           yaxis = list(title = vars[2]),
+                                           zaxis = list(title = vars[3])))
+  # print(d3)
   htmlwidgets::saveWidget(d3, out.plot)
   print(paste("3D interactive plot created in:", out.plot))
 }
